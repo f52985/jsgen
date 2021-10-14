108c1d9aee41d82b25184cba7e78dad3b4dbf168
DJ <f52985@gmail.com>
2021-10-14 21:32:02 +0900

    Remove unnecessary feature constructor

--- src/main/scala/kr/ac/kaist/jsgen/feature/Feature.scala
+++ src/main/scala/kr/ac/kaist/jsgen/feature/Feature.scala
@@ -3,9 +3,3 @@ package kr.ac.kaist.jsgen.feature
 case class Feature(feat: String) {
   override def toString() = feat
 }
-
-object Feature {
-  def apply(vec: FeatureVector): Feature = {
-    Feature("Script")
-  }
-}
