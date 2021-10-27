850799e6b8bb0476240aea2de14ef155d6452615
DJ <f52985@gmail.com>
2021-10-27 15:34:41 +0900

    Filter script files that pass jiset eval

--- lib/desugar/filter.sh
+++ lib/desugar/filter.sh
@@ -6,7 +6,7 @@ rm -rf module
 test262TestDir="../../tests/test262/test"
 prefixLen=25
 
-echo "Creating directory"
+echo "Creating directory.."
 for d in `find $test262TestDir -type d`; do
   d1="script/${d:$prefixLen}"
   d2="module/${d:$prefixLen}"
@@ -15,17 +15,22 @@ for d in `find $test262TestDir -type d`; do
   mkdir $d2
 done
 
-echo "Discriminating file"
-for f in `find $test262TestDir -name "*.js"`; do
-  if grep -q "type: SyntaxError" "$f" ; then
-    continue
-  fi
+echo "Copying.."
+while read line; do
+  cp $test262TestDir/$line script/$line
+done < filter.txt
 
-  if grep -q "flags: \[.*module.*\]" $f || [[ "$f" == *"_FIXTURE.js" ]] ; then
-    cp $f "module/${f:$prefixLen}"
-  else
-    cp $f "script/${f:$prefixLen}"
-  fi
-done
+#echo "Discriminating file"
+#for f in `find $test262TestDir -name "*.js"`; do
+#  if grep -q "type: SyntaxError" "$f" ; then
+#    continue
+#  fi
+#
+#  if grep -q "flags: \[.*module.*\]" $f || [[ "$f" == *"_FIXTURE.js" ]] ; then
+#    cp $f "module/${f:$prefixLen}"
+#  else
+#    cp $f "script/${f:$prefixLen}"
+#  fi
+#done
 
 date +%"T"
